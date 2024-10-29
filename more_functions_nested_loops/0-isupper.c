894a4ea3b0c544c5345e37c0647fd50329687739
Alank196 <9773@holbertonstudents.com>
2024-10-29 14:52:30 +0000

    0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -5,10 +5,10 @@
  * @x: The number to be checked
  * Return: 1 for upper letter or 0 for any else
  */
-int _isupper(int x)
+int _isupper(int c)
 {
 
-	if (x >= 65 && x <= 98)
+	if (c >= 'A' && c <= 'Z')
 	{
 	return (1);
 	}
